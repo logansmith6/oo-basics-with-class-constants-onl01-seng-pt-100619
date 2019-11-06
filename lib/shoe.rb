4c4bafb0a932e8ee4d1e7e3587498a670d20ebf0
Logan Smith <logsmith06@gmail.com>
2019-11-06 21:27:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,8 +14,5 @@ class Shoe
   end
 
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  
 end
