061be83303a6d86bdbc3e2a8b7b30ecab7e0d3ff
Logan Smith <logsmith06@gmail.com>
2019-11-06 21:21:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   GENRES = []
@@ -13,4 +13,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
