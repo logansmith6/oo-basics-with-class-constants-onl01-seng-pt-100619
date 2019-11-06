c177983fce1284312f298f2acdaa607e91f0c7e9
Logan Smith <logsmith06@gmail.com>
2019-11-06 21:25:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-
+  attr_reader :brand
 
   BRANDS = []
   def initialize(brand)
@@ -13,7 +13,7 @@ class Shoe
   end
 
 
-  def brands=(brand)
+  def brand=(brand)
     @brand = brand
     BRANDS << brand
   end
